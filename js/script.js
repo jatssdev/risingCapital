0a9381eeb1d73240a60e2b85b215d4e5a5b3c509
jatssdev <167951806+jatssdev@users.noreply.github.com>
2024-09-13 12:28:32 +0530

    ..

--- js/script.js
+++ js/script.js
@@ -1,5 +1,108 @@
 /* ---- particles.js config ---- */
 
+// header start 
+const menu = document.querySelector(".menu");
+const header = document.querySelector("#header");
+const menuInner = menu.querySelector(".menu__inner");
+const menuArrow = menu.querySelector(".menu__arrow");
+const menuTitle = menu.querySelector(".menu__title");
+const burger = document.querySelector(".burger");
+const overlay = document.querySelector(".overlay");
+
+// Navbar Menu Toggle Function
+function toggleMenu() {
+    menu.classList.toggle("is-active");
+    overlay.classList.toggle("is-active");
+    header.classList.toggle("is-active");
+}
+
+// Show Mobile Submenu Function
+function showSubMenu(children) {
+    let subMenu = children.querySelector(".submenu");
+    if (subMenu) {
+        subMenu.classList.add("is-active");
+        subMenu.style.animation = "slideLeft 0.35s ease forwards";
+        const titleText = children.querySelector(".menu__link").textContent.trim();
+        menuTitle.textContent = titleText;
+        menu.querySelector(".menu__header").classList.add("is-active");
+    }
+}
+
+// Hide Mobile Submenu Function
+function hideSubMenu() {
+    let subMenu = menu.querySelector(".submenu.is-active");
+    if (subMenu) {
+        subMenu.style.animation = "slideRight 0.35s ease forwards";
+        setTimeout(() => {
+            subMenu.classList.remove("is-active");
+        }, 300);
+
+        menuTitle.textContent = "";
+        menu.querySelector(".menu__header").classList.remove("is-active");
+    }
+}
+
+// Toggle Mobile Submenu Function
+function toggleSubMenu(e) {
+    if (!menu.classList.contains("is-active")) {
+        return;
+    }
+    if (e.target.closest(".menu__dropdown")) {
+        const children = e.target.closest(".menu__dropdown");
+        showSubMenu(children);
+    }
+}
+
+// Fixed Navbar Menu on Window Resize
+window.addEventListener("resize", () => {
+    if (window.innerWidth >= 768) {
+        if (menu.classList.contains("is-active")) {
+            toggleMenu();
+        }
+    }
+});
+
+// Dark and Light Mode with localStorage
+(function () {
+    let darkMode = localStorage.getItem("darkMode");
+    const darkSwitch = document.getElementById("switch");
+
+    // Enable and Disable Darkmode
+    const enableDarkMode = () => {
+        document.body.classList.add("darkmode");
+        localStorage.setItem("darkMode", "enabled");
+    };
+
+    const disableDarkMode = () => {
+        document.body.classList.remove("darkmode");
+        localStorage.setItem("darkMode", null);
+    };
+
+    // User Already Enable Darkmode
+    if (darkMode === "enabled") {
+        enableDarkMode();
+    }
+
+    // User Clicks the Darkmode Toggle
+    darkSwitch.addEventListener("click", () => {
+        darkMode = localStorage.getItem("darkMode");
+        if (darkMode !== "enabled") {
+            enableDarkMode();
+        } else {
+            disableDarkMode();
+        }
+    });
+})();
+
+// Initialize All Event Listeners
+burger.addEventListener("click", toggleMenu);
+overlay.addEventListener("click", toggleMenu);
+menuArrow.addEventListener("click", hideSubMenu);
+menuTitle.addEventListener("click", hideSubMenu);
+menuInner.addEventListener("click", toggleSubMenu);
+
+// header end
+
 
 particlesJS("particles-js", {
     "particles": {
